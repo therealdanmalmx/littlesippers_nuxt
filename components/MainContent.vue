541de3940b1ad280b9065083488deadb01cdd8e7
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-10-08 12:19:46 +0200

    Add icons. refactor setup script

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -1,37 +1,36 @@
 <template>
-  <main class="flex-grow">
-    <div style="height: 70vh;">
+    <div style="height: 75vh;">
         <LMap
             ref="map"
             :zoom="zoom"
-            :center="[47.21322, -1.559482]"
+            :center="[57.721001, 12.94025]"
         >
             <LTileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                attribution="&amp;copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/attributions'>CARTO</a>"
                 layer-type="base"
                 name="OpenStreetMap"
             />
         </LMap>
     </div>
-    <div class="bg-white p-6" style="height: 30vh;">
-      <div class="h-full flex justify-center items-center">
-        <input type="text" placeholder="Enter location..." class="border rounded-l px-4 py-2 w-1/2">
-        <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-r">Search</button>
-      </div>
+    <div class="bg-white p-6" style="height: 25vh;">
+        <div class="flex justify-center items-center">
+            <input type="text" placeholder="Enter location..." class="border rounded-l px-4 py-2 w-1/2">
+            <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-r">Search</button>
+        </div>
+        <div class="flex justify-center items-end w-full mt-8">
+            <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-r">Add a new location</button>
+        </div>
     </div>
-  </main>
 </template>
 
-<script>
-
-export default {
-  name: "MainContent",
-  mounted() {
-    // const map = Leaflet.map(this.$refs.map).setView([51.505, -0.09], 13);
-    // Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(map);
-  }
-};
+<script setup>
+import { ref } from 'vue'
+const zoom = ref(10)
 </script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="bg-gray-100 flex flex-col min-h-screen">
-      <NavBar />
       <MainContent />
       <FooterBar />
     </div>
   </template>
 
   <script>
-  import NavBar from "../components/NavBar.vue";
   import MainContent from "../components/MainContent.vue";
   import FooterBar from "../components/FooterBar.vue";
 
   export default {
     components: {
-      NavBar,
       MainContent,
       FooterBar,
     },
